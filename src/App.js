5d588e7373ab5de3991c77b842b3a8e60362cee7
Andres Carreon <andresomar95@gmail.com>
2020-12-31 12:43:12 -0600

    slow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload part 2. Holaaa Cassandra :D
+          Edit <code>src/App.js</code> and save to reload part 2. Holaaa Cassandra :D sloow
         </p>
         
         <a
